4074aca5a387df9671fe912faaf81dd2c51c4e06
Darkmiclos <Mikls@DESKTOP-E6I7RQ4>
2021-10-12 13:40:01 +0200

    Add gallery test,view and template. Modify navbar.scss for correct href.

--- pages/tests/test_urls.py
+++ pages/tests/test_urls.py
@@ -4,3 +4,7 @@ class URLTests(TestCase):
     def test_testhomepage(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_gallery(self):
+        response = self.client.get('/gallery/')
+        self.assertEqual(response.status_code, 200)
--- web_project/urls.py
+++ web_project/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home
+from pages.views import home, gallery
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', home, name = "home"),
+    path('', home, name = 'home'),
+    path('gallery/', gallery, name = 'gallery'),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
