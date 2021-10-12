74a74b7b441a92c0420e8b6c8a5d213e24ffc744
Darkmiclos <Mikls@DESKTOP-E6I7RQ4>
2021-10-12 12:42:54 +0200

    Add responsive mobile/tablet design for topnav.

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,1 +1,15 @@
-console.log("It works!");
+window.onload = function() {
+    var hamburgerMenu = document.querySelector(".hamburger");
+    var navMenu = document.querySelector(".nav__links");
+
+    if(hamburgerMenu)
+    {
+        hamburgerMenu.addEventListener("click", mobileMenu);
+    }
+    
+
+    function mobileMenu() {
+        hamburgerMenu.classList.toggle("is__active");
+        navMenu.classList.toggle("is__active");
+    }
+}
