d5fcbe78bb6bb53ac305a4347bd41220b12a8116
Lockár Miklós <darkmiclos2002002@gmail.com>
2021-11-07 14:41:58 +0100

    Add image popup when an image is clicked.

---
+++ static/javascript/modules/image_clicked.js
@@ -0,0 +1,46 @@
+let imgPopup = document.getElementsByClassName("img-clicked");
+console.log(imgPopup);
+
+if(imgPopup)
+{
+  
+  for(let i = 0; i < imgPopup.length; i++)
+  {
+    imgPopup[i].addEventListener("click", () => {
+      popUp(i);
+    });
+  }
+}
+
+function popUp(n)
+{
+  // Creating elements
+  let imgPopupContainer = document.createElement("div");
+  let closeButtonSpanLeft = document.createElement("span");
+  let closeButtonSpanRight = document.createElement("span");
+  let img = document.createElement("img");
+  // Initializing my new img
+  img.src = imgPopup[n].src;
+  // Appending the elements to the body
+  document.body.appendChild(imgPopupContainer);
+  imgPopupContainer.append(img, closeButtonSpanLeft, closeButtonSpanRight);
+  //Creating the necessary classes and ids for scss
+  imgPopupContainer.classList.add("img-popup-container");
+  closeButtonSpanLeft.classList.add("close-button");
+  closeButtonSpanLeft.id = "close-button-left";
+  closeButtonSpanRight.id = "close-button-right";
+  closeButtonSpanRight.classList.add("close-button");
+  img.classList.add("img-popup");
+  // Add event listener to close button
+  closeButton = document.getElementsByClassName("close-button");
+  if(closeButton)
+  {
+    for(var i = 0; i < closeButton.length; i++)
+    {
+      closeButton[i].addEventListener("click", () => {
+        // Remove popUp
+        document.body.removeChild(imgPopupContainer);
+      });
+    }
+  }
+}
